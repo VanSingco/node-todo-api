1d1bac26a590d580cc9e9d657f77034e613ebe1e
Van Zachary Singco19 <edwincarneses@gmail.com>
2018-06-09 17:34:09 +0800

    add GET / todos routes

--- server/server.js
+++ server/server.js
@@ -20,7 +20,15 @@ app.post('/todos', (req, res) => {
    }).catch((err) => {
        res.status(400).send(err);
    });
-})
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }).catch((err) => {
+         res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
