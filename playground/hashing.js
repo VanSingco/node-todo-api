c133d7b6f8c6dd8feb4c77605b2a022145801cd0
Van Zachary Singco19 <edwincarneses@gmail.com>
2018-06-13 11:25:44 +0800

    adding password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs')
+
+const password = 'singco143';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash)
+    });
+});
+
+const hashedPassword = '$2a$10$vvM9uNBHg9pWBUl5vTVZnOKJQLMRua2nOuxcTExS.O.MAmea8Mr7e'
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+})
 // const message = "I am user number 1";
 // const hash = SHA256(message).toString();
 
 // console.log(hash)
 
-const data = {
-    id: 10
-}
+// const data = {
+//     id: 10
+// }
 
-const token = jwt.sign(data, 'singco');
-const decoded = jwt.verify(token, 'singco')
+// const token = jwt.sign(data, 'singco');
+// const decoded = jwt.verify(token, 'singco')
 
-console.log(`Token: ${token}`)
-console.log('Decoded', decoded)
+// console.log(`Token: ${token}`)
+// console.log('Decoded', decoded)
